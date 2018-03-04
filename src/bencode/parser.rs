0d5bb7180dc6a4b0c8f7f1420b55750d0c12db47
藤野 宏樹 <fujino@rss0041npc.local>
2018-03-04 20:36:07 +0900

    BenStringまで実装
    test実装整理

--- src/bencode/parser.rs
+++ src/bencode/parser.rs
@@ -1,6 +1,7 @@
-/*
+use bencode::code_type::*;
+
 // BenCodeのパーサー
-trait BenCodeParser {
+pub trait BenCodeParser {
     // BenCodeにencodeする
     fn encode(&self) -> BenCode;
     // BenCodeをdecodeする
@@ -8,36 +9,15 @@ trait BenCodeParser {
 }
 
 impl BenCodeParser for BenString {
-    pub fn encode(&self) -> BenCode {
-        let length = self.value.len();
-        let mut code = Vec::with_capacity(2 + length);
-        code.push(length.to_str());
-        code.push(":");
-        code.extend_from_slice(&self.value[2..length]);
-        BenCode {
-            value: code
-        }
+    fn encode(&self) -> BenCode {
+        let length = self.get_value().len();
+        let code = length.to_string() + ":" + &self.get_value();
+        BenCode::create(code)
     }
-    pub fn decode(ben_code: BenCode) -> BenString {
-        let string_value = Vec::with_capacity(ben_code.getValue().len()-2);
-        string_value.extend_from_slice(&string_value);
-        BenString {
-            value: string_value
-        }
+
+    fn decode(ben_code: BenCode) -> BenString {
+        let length = ben_code.get_value().len();
+        let string_value = String::from(&ben_code.get_value()[2..length]);
+        BenString::new(string_value)
     }
 }
-
-impl BenCodeParser for BenNum {
-    fn encode(&self) -> BenCode {}
-    fn decode(ben_code: BenCode) -> BenString {}
-}
-
-impl BenCodeParser for BenList<T> {
-    fn encode(&self) -> BenCode {}
-    fn decode(ben_code: BenCode) -> BenList<T> {}
-}
-
-impl BenCodeParser for BenNum {
-    fn encode(&self) -> BenCode {}
-    fn decode(ben_code: BenCode) -> BenNum {}
-}*/
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
 mod bencode;
+use bencode::parser::BenCodeParser;
 
 fn main() {
 
-    let mut test_vec = Vec::new();
-    test_vec.push(52); // 4
-    test_vec.push(58); // :
-    test_vec.push(111); // o
-    test_vec.push(100); // d
-    test_vec.push(101); // e
-    test_vec.push(110); // n
-
-    // input string
-    let input_string = test_vec.iter().map(|&s| s as char).collect::<String>();
-    println!("input bencode: {:?}", input_string);
-
+    let input_string = String::from("oden");
+    println!("input string: {:?}", input_string); // 4:oden
     // BenCode
-    let ben_code = bencode::code_type::BenCode::new(test_vec);
+    //let ben_code = bencode::code_type::BenCode::new(input_string.as_bytes());
+    let ben_string = bencode::code_type::BenString::new(input_string);
+    let ben_code = bencode::code_type::BenString::encode(&ben_string);
+    println!("bencode: {:?}", ben_code); // 4:oden
 
     // BenString
-    let ben_string = bencode::code_type::BenString::decode(ben_code);
-    println!("decodeed bencode: {:?}", ben_string);
+    let decodeed_ben_string = bencode::code_type::BenString::decode(ben_code);
+    println!("decodeed bencode: {:?}", ben_string); // oden
 }
